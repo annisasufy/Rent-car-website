478a0573b0c1236a8451668289236c2f198ab7a5
Annisa Uswa Sufia <annisauswasufia@gmail.com>
2022-11-04 16:15:20 +0700

    revisi alamat dan testimoni

--- src/component/Tes.jsx
+++ src/component/Tes.jsx
@@ -46,6 +46,7 @@ const Tes = () => {
         slidesToScroll: 3,
         initialSlide: 1,
         arrows: false,
+        autoplay: true,
 
         responsive: [
             {
@@ -79,10 +80,10 @@ const Tes = () => {
         <div className='mt-20 md:my-20 md:h-40 ' id='Testimoni'>
             <h1 className='font-bold text-center text-3xl lg:text-5xl mb-11 stroke-2'>Testimoni <p className='font-normal text-sm'>Apa yang orang katakan kepada kami</p></h1>
             <div className="flex">
-                <div className="w-[340px] sm:w-[500px] md:w-[600px] lg:w-[1100px] max-w-[1024px] mx-auto z-10">
+                <div className="w-[340px] sm:w-[500px] md:w-[600px] lg:w-[1100px] max-w-[1024px] mx-auto z-10 ">
                     <Slider{...settings}>
                         {dataTestimoni.map((item)=>(
-                            <div className=' transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 hover:shadow-orange/30  duration-300 p-10 rounded-2xl bg-white font-poppins text-[#777777] shadow-xl'>
+                            <div className='mt-3 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 hover:shadow-orange/30  duration-300 p-10 rounded-2xl bg-white font-poppins text-[#777777] shadow-xl'>
                                 <div className='grid grid-cols-2 justify-items-start'>
                                     <div className="flex gap-2">
                                         <img className='' src={item.photo} alt=''/>
